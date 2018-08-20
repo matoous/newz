bae582e0892d689ced1cc8abed2ccc8df86e7191
Matous Dzivjak <dzivjmat@fel.cvut.cz>
2018-08-20 23:01:12 +0200

    change place for config

--- news/__init__.py
+++ news/__init__.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 from news.config.app import make_app
 from dotenv import load_dotenv
 
-load_dotenv("/etc/opt/news")
+env_path = Path('..') / '.env'
+load_dotenv(dotenv_path=env_path)
 app = make_app()
