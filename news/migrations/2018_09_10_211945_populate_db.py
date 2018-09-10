c0aca9f0efc67df3f8ee4d269fcd77adc5ae3402
Matous Dzivjak <dzivjmat@fel.cvut.cz>
2018-09-10 23:26:26 +0200

    populate db with data

---
+++ news/migrations/2018_09_10_211945_populate_db.py
@@ -0,0 +1,28 @@
+import random
+import string
+
+from orator.migrations import Migration
+from passlib.handlers.bcrypt import bcrypt
+
+
+class PopulateDb(Migration):
+
+    def up(self):
+        """
+        Run the migrations.
+        """
+        self.db.table('users').insert([
+            {'username': 'autoposter',
+             'password': bcrypt.hash(''.join(random.choices(string.ascii_uppercase + string.digits, k=32))), id: 12345,
+             'email': 'autoposter'},
+            {'username': 'admin',
+             'password': bcrypt.hash(''.join(random.choices(string.ascii_uppercase + string.digits, k=32))),
+             'email': 'admin@esourcenews.com'},
+        ])
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.db.table('users').where('id', '=', 12345).delete()
+        self.db.table('users').where('username', '=', 'admin').delete()
